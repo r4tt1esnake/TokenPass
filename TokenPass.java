dbdd4daf601d7bfe31e2e49bb6fe066384dbb792
r4tt1esnake <r4tt1esnake@users.noreply.github.com>
2017-12-15 17:48:57 -0800

    Initial commit

---
+++ TokenPass.java
@@ -0,0 +1,19 @@
+class TokenPass {
+
+	private int board[];
+	private int currentPlayer;
+
+	public TokenPass(int playerCount) {
+
+		board = new int[playerCount];
+		for(int i = 0; i < board.length; i++) {
+			
+		}
+
+	}
+
+	public void distributeCurrentPlayerTokens() {
+
+	}
+
+}
